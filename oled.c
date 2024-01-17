0dc1d2645ed38850e4680ca6000543878303bc79
Tony Do <tonyttdo@gmail.com>
2024-01-17 15:17:00 -0600

    chore: updates to keymap, dictionary, and rules

--- oled.c
+++ oled.c
@@ -1,3 +1,11 @@
 #include QMK_KEYBOARD_H
 
-
+bool apply_autocorrect(uint8_t backspaces, const char *str, char *typo, char *correct) {
+#ifdef OLED_ENABLE
+    oled_write_P(PSTR("Auto-corrected"), false);
+#endif
+#ifdef CONSOLE_ENABLE
+    printf("'%s' was corrected to '%s'\n", typo, correct);
+#endif
+    return true;
+}
